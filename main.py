910dd67b8f60d4c5f584017f886765c1c56457d2
iradukundapaci <paifique@gmail.com>
2024-05-29 17:13:19 +0200

    handling the main excution file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 
 from datetime import datetime, timedelta
+from Data_handling_scripts.DataHandler import DataHandler
 from Data_handling_scripts.WebScraper import Scraper
 
 # from .DataHandler import DataHandler
@@ -32,21 +33,19 @@ def download_report(user_email, password, plate_number):
     return True
 
 
-# def clean_report(file_path):
-#     logging.info("Starting report cleaning...")
-#     try:
-#         handler = DataHandler()
-#         if os.path.exists(file_path):
-#             df = pd.read_csv(file_path)
-#             handler.process_daily_data(df, file_path)
-#             logging.info("Report cleaning completed.")
-#         else:
-#             logging.error(f"File {file_path} does not exist.")
-#             return False
-#     except Exception as e:
-#         logging.error(f"An error occurred during report cleaning: {e}")
-#         return False
-#     return True
+def clean_report():
+    logging.info("Starting report cleaning...")
+    try:
+        handler = DataHandler()
+        if handler.rename_and_clean_file():
+            logging.info("Report cleaning completed.")
+        else:
+            logging.error("Report cleaning failed")
+            return False
+    except Exception as e:
+        logging.error(f"An error occurred during report cleaning: {e}")
+        return False
+    return True
 
 
 def main():
@@ -56,19 +55,13 @@ def main():
         plate_number = "RAC 151 S"  # Replace with actual plate number
 
         download_success = download_report(user_email, password, plate_number)
-        # if download_success:
-        #     handler = DataHandler()
-        #     file_path = os.path.join(
-        #         handler.CLEANED_DATA_FOLDER, "example.csv"
-        #     )  # Update with the correct file path
-        #     clean_success = clean_report(file_path)
-        #     if clean_success:
-        #         send_whatsapp_message()
-        #         logging.info("All tasks completed successfully.")
-        #     else:
-        #         logging.error("Report cleaning failed.")
-        # else:
-        #     logging.error("Report download failed.")
+        if download_success:
+            if clean_report():
+                logging.info("All tasks completed successfully.")
+            else:
+                logging.error("Report cleaning failed.")
+        else:
+            logging.error("Report download failed.")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
